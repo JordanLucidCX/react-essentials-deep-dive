8e8f3f0befe080f2a3e33c453e5e487973d65b3b
Jordan Baker <Jordan.Baker@likewize.com>
2024-11-18 21:50:56 +0000

    Player component created

---
+++ src/components/Player/Player.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+
+function Player({initialName, symbol}) {
+    const [playerName, setPlayerName] = useState(initialName);
+    const [isEditing, setIsEditing] = useState(false);
+
+    function handleChange(event) {
+        setPlayerName(event.target.value);
+    }
+
+    const editablePlayerName = isEditing ? <input type="text" required value={playerName} onChange={handleChange}></input> : <span className="player-name">{playerName}</span>;
+    const buttonText = isEditing ? "Save" : "Edit";
+
+    return (
+        <li>
+            <span className="player">
+              {editablePlayerName}
+              <span className="player-symbol">{symbol}</span>
+            </span>
+            <button onClick={() => setIsEditing(editing => !editing)}>{buttonText}</button>
+        </li>
+    );
+}
+
+export default Player;
